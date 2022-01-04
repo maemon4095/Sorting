288d47f3195a411e001acee931a95d1886da0b4e
maemon <47819358+maemon4095@users.noreply.github.com>
2022-01-04 22:56:14 +0900

    dynamic js runtime

---
+++ Sorting/DynamicJSRuntime.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.JSInterop;
+using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Sorting;
+
+public class DynamicJSRuntime : IDynamicMetaObjectProvider
+{
+    readonly IJSRuntime runtime;
+    readonly string prefix;
+
+    public DynamicJSRuntime(IJSRuntime runtime) : this(runtime, string.Empty) { }
+    DynamicJSRuntime(IJSRuntime runtime, string prefix) { (this.runtime, this.prefix) = (runtime, prefix); }
+
+    public DynamicMetaObject GetMetaObject(Expression parameter) => new MetaObject(parameter, this);
+
+    private string Qualify(string path)
+    {
+        if (string.IsNullOrEmpty(this.prefix)) return path;
+
+        return $"{this.prefix}.{path}";
+    }
+
+    private DynamicJSRuntime WithPrefix(string prefix)
+    {
+        return new DynamicJSRuntime(this.runtime, this.Qualify(prefix));
+    }
+    private Task<T> InvokeAsync<T>(string identity, object[] args) => this.runtime.InvokeAsync<T>(this.Qualify(identity), args).AsTask();
+    private Task<T> InvokeAsync<T>(object[] args) => this.runtime.InvokeAsync<T>(this.prefix, args).AsTask();
+
+    class MetaObject : DynamicMetaObject
+    {
+        readonly Expression runtimeExpr;
+        readonly MethodInfo invokeMethod;
+        readonly MethodInfo invokeSelfMethod;
+        readonly MethodInfo withPrefixMethod;
+
+        internal MetaObject(Expression expression, DynamicJSRuntime runtime) : this(expression, BindingRestrictions.Empty, runtime) { }
+
+        private MetaObject(Expression expression, BindingRestrictions restrictions, DynamicJSRuntime dynamicRuntime)
+             : base(expression, restrictions, dynamicRuntime)
+        {
+            this.runtimeExpr = this.Limit(Expression.Convert(this.Expression, typeof(DynamicJSRuntime)));
+            this.invokeMethod = typeof(DynamicJSRuntime).GetMethod(nameof(DynamicJSRuntime.InvokeAsync), BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(string), typeof(object[]) })!;
+            this.invokeSelfMethod = typeof(DynamicJSRuntime).GetMethod(nameof(DynamicJSRuntime.InvokeAsync), BindingFlags.NonPublic | BindingFlags.Instance, new[] { typeof(object[]) })!;
+            this.withPrefixMethod = typeof(DynamicJSRuntime).GetMethod(nameof(DynamicJSRuntime.WithPrefix), BindingFlags.NonPublic | BindingFlags.Instance)!;
+
+        }
+
+        private Expression WithPrefix(Expression expr, string prefix) => Expression.Call(expr, this.withPrefixMethod, Expression.Constant(prefix));
+        private Expression Limit(Expression expr) => Expression.Convert(expr, this.LimitType);
+
+        public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
+        {
+            var meta = new DynamicMetaObject(
+                    this.WithPrefix(this.runtimeExpr, binder.Name),
+                    BindingRestrictions.GetTypeRestriction(this.Expression, this.LimitType));
+            return meta;
+        }
+
+        public override DynamicMetaObject BindInvokeMember(InvokeMemberBinder binder, DynamicMetaObject[] args)
+        {
+            var invokeExpr = Expression.Convert(
+                        Expression.Call(
+                            this.runtimeExpr,
+                            this.invokeMethod.MakeGenericMethod(binder.ReturnType),
+                            Expression.Constant(binder.Name),
+                            Expression.NewArrayInit(typeof(object), args.Select(arg => arg.Expression))
+                        ),
+                        binder.ReturnType
+                    );
+
+            return new(Expression.Convert(invokeExpr, binder.ReturnType), BindingRestrictions.GetTypeRestriction(this.Expression, this.LimitType));
+        }
+
+        public override DynamicMetaObject BindInvoke(InvokeBinder binder, DynamicMetaObject[] args)
+        {
+            var invokeExpr = Expression.Convert(
+                Expression.Call(
+                        this.runtimeExpr,
+                        this.invokeSelfMethod.MakeGenericMethod(binder.ReturnType),
+                        Expression.NewArrayInit(typeof(object), args.Select(arg => arg.Expression))
+                    ),
+                    binder.ReturnType
+                );
+
+            return new(Expression.Convert(invokeExpr, binder.ReturnType), BindingRestrictions.GetTypeRestriction(this.Expression, this.LimitType));
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -8,12 +8,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services
-    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-    .AddScoped<DimensionService>()
-    .AddScoped<LocalStorageService>()
-    .AddMessagePipe();
-var sp = builder.Services.BuildServiceProvider();
+var services = builder.Services;
+
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+        .AddScoped<DimensionService>()
+        .AddScoped<LocalStorageService>()
+        .AddMessagePipe();
+var sp = services.BuildServiceProvider();
 GlobalMessagePipe.SetProvider(sp);
 
 await builder.Build().RunAsync();
